3f9e1c88c3cdb561b114005e0d6a8628bc231779
Nova <xplodnstar@live.com>
2019-04-08 15:59:50 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import archer from './archer.gif'
+import still from './archer.jpg'
+import './App.css'
 
 class App extends Component {
+  state = {
+    gulp: 0,
+    tag: 'gulps',
+    archer: still
+  }
+
+  handleClick = () => {
+    if (this.state.gulp === 0) {
+      this.setState({
+        tag: 'gulp'
+      })
+    } else {
+      this.setState({
+        tag: 'gulps'
+      })
+    }
+    let drinks = Number(this.state.gulp + 1)
+    this.setState({
+      gulp: drinks,
+      archer: archer
+    })
+
+    setTimeout(() => {
+      this.setState({
+        archer: still
+      })
+    }, 3000)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="isis">
+        <img src={this.state.archer} className="App-gif" alt="Archer" id="gif" />
+        <p>
+          Click to take a drink.
+        </p>
+        <button onClick={this.handleClick}>{this.state.gulp} {this.state.tag}</button>
+      </div >
     );
   }
 }
